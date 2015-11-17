b1030b44f100c78ca5e5a7fcf0614907ebf2c688
olesya <kots.olesya@gmail.com>
2015-11-17 22:38:42 +0200

    JavaScript 1

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//https://learn.javascript.ru/variable-names
+alert( 'Я – Olesya Kots!' );
+alert('сама красива');
+//змінні
+var masseg;
+masseg = 'Изучение какого-нибудь функционального языка рекомендуется для расширения кругозора'
+alert(masseg);
+//константи
+var COLOR_ORANGE = "#FF7F00";
+
+var color = COLOR_ORANGE;
+alert( color ); 
+
+
